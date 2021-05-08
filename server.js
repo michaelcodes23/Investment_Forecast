9b83ce08bd099b5e1a92f3fc7839733f5109f391
Michael <cuencamich@hotmail.com>
2021-05-08 15:22:44 -0400

    Commiting all my files with .json in the root directory for better Heroku connection

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+require('dotenv').config();
 const methodOverride = require('method-override');
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const db = mongoose.connection;
 const URI = process.env.MONGODB_URI;
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 //adding a method override to allow us to delete
 app.use(methodOverride('_method'));
+
 //CSS
 app.use(express.static('public'));
 
@@ -22,13 +24,17 @@ app.use('/invest',invtController);
 //Connections
 
 //everytime I use captlogs everything shows
-app.listen(port,()=>{
-    console.log('listening on port', port)
-})
 
-mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true })
-
-mongoose.connection.once('open', () => {
-    console.log('connected to mongo');
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 });
-
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to mongo')
+});
+app.listen(PORT, () => {
+    console.log('listening on port', PORT)
+})
